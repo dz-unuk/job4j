32a0a4057cbdaa7b8ca9738ecaf3803a7b090880
dz-unuk <d.unuchak@gmail.com>
2018-12-05 17:15:15 +0300

    1.4. JUnit[#93004]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*Test.
+*@author Denis Unuchak
+*@since 05.12.2018
+*@version 0.0
+*/
+public class CalculateTest{
+	/** 
+	*Test echo.
+	*/
+	@Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+		String input = "Petr Arsentev";
+		String expect = "Echo, echo, echo : Petr Arsentev"; 
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+}
