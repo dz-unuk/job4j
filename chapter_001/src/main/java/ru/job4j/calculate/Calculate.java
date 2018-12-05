604fafe0a07fe153f6265f53e3765970e974d012
dz-unuk <d.unuchak@gmail.com>
2018-12-05 14:21:13 +0300

    1.3. JavaDoc. Code convention[#93003]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,15 @@
 package ru.job4j.calculate;
+/**
+*Будет калькулятор.
+*@author Denis Unuchak
+*@since 05.12.2018
+*@version 0.0
+*/
 public class Calculate{
+	/** 
+	*Конструктор.
+	*@param arg - arg.
+	*/
 	public static void main (String[] args){
 		System.out.println("Hello world!");
 	}
