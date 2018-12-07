8e9d6e3a6a343f67858a9e8cb4dc9166f97b6f4f
dz-unuk <d.unuchak@gmail.com>
2018-12-07 23:54:05 +0300

    4.4. Максимум из трех чисел - в одну строку

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -25,8 +25,6 @@ public class Max {
      * @return большее из аргументов
      */
     public int max(int first, int second, int third) {
-        int temp = this.max(first, second);
-        temp = this.max(temp, third);
-        return temp;
+        return this.max(first, this.max(second, third));
     }
 }
