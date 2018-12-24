e44d55b1762b22cddc932468088ec679ca7ca83f
dz-unuk <d.unuchak@gmail.com>
2018-12-24 21:13:05 +0300

    Вопросы.[#93010] Задача о слиянии двух отсортированных массивов. Решение: ExamEx в пакете exam. Добавлен тест с разными длинами входных массивов.

--- chapter_001/src/test/java/ru/job4j/exam/ExamExTest.java
+++ chapter_001/src/test/java/ru/job4j/exam/ExamExTest.java
@@ -21,4 +21,13 @@ public class ExamExTest {
         int[] result = check.concatSortArray(input1, input2);
         assertThat(result, is(expected));
     }
+    @Test
+    public void whenChekConcatDiffLenght() {
+        ExamEx check = new ExamEx();
+        int[] input1 = new int[]{1, 3, 9, 11};
+        int[] input2 = new int[]{2, 4, 6, 8, 10, 12};
+        int[] expected = new int[]{1, 2, 3, 4, 6, 8, 9, 10, 11, 12};
+        int[] result = check.concatSortArray(input1, input2);
+        assertThat(result, is(expected));
+    }
 }
