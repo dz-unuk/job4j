a7a29cfbfe025e0095469e77a2c5b5e3f79769e2
dz-unuk <d.unuchak@gmail.com>
2019-01-24 13:19:09 +0300

    Chess. Исправил ошибки и qeen->queen. Запускается и проходит verify

--- chess/src/main/java/ru/job4j/chess/firuges/black/QueenBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/QueenBlack.java
@@ -1,4 +1,4 @@
-package ru.job4j.chess.firuges.white;
+package ru.job4j.chess.firuges.black;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
@@ -9,10 +9,10 @@ import ru.job4j.chess.firuges.Figure;
  * @version $Id$
  * @since 0.1
  */
-public class QeenWhite implements Figure {
+public class QueenBlack implements Figure {
     private final Cell position;
 
-    public QeenWhite(final Cell position) {
+    public QueenBlack(final Cell position) {
         this.position = position;
     }
 
@@ -23,11 +23,11 @@ public class QeenWhite implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        return new Cell[] { dest };
+        return new Cell[] {dest};
     }
 
     @Override
     public Figure copy(Cell dest) {
-        return new QeenWhite(dest);
+        return new QueenBlack(dest);
     }
 }
--- chess/src/main/java/ru/job4j/chess/firuges/white/QueenWhite.java
+++ chess/src/main/java/ru/job4j/chess/firuges/white/QueenWhite.java
@@ -1,4 +1,4 @@
-package ru.job4j.chess.firuges.black;
+package ru.job4j.chess.firuges.white;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
@@ -9,10 +9,10 @@ import ru.job4j.chess.firuges.Figure;
  * @version $Id$
  * @since 0.1
  */
-public class QeenBlack implements Figure {
+public class QueenWhite implements Figure {
     private final Cell position;
 
-    public QeenBlack(final Cell position) {
+    public QueenWhite(final Cell position) {
         this.position = position;
     }
 
@@ -23,11 +23,11 @@ public class QeenBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        return new Cell[] { dest };
+        return new Cell[] {dest};
     }
 
     @Override
     public Figure copy(Cell dest) {
-        return new QeenBlack(dest);
+        return new QueenWhite(dest);
     }
 }
