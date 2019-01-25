8cdbb03e0c30b19a2bc0e7f16a1bac60d18e222e
dz-unuk <d.unuchak@gmail.com>
2019-01-25 16:50:51 +0300

    Revert "Chess. Исправил ошибку - слон ходит нормально." Реализовал движение коня

--- chess/src/main/java/ru/job4j/chess/figures/black/KnightBlack.java
+++ chess/src/main/java/ru/job4j/chess/figures/black/KnightBlack.java
@@ -2,6 +2,7 @@ package ru.job4j.chess.figures.black;
 
 import ru.job4j.chess.figures.Cell;
 import ru.job4j.chess.figures.Figure;
+import ru.job4j.chess.figures.ImpossibleMoveException;
 
 /**
  *
@@ -22,7 +23,14 @@ public class KnightBlack implements Figure {
     }
 
     @Override
-    public Cell[] way(Cell source, Cell dest) {
+    public Cell[] way(Cell source, Cell dest) throws ImpossibleMoveException {
+        int xDist = Math.abs(dest.x - source.x);
+        int yDist = Math.abs(dest.y - source.y);
+        System.out.println(xDist);
+        System.out.println(yDist);
+        if (!((xDist == 1 & yDist == 2) || (xDist == 2 & yDist == 1))) {
+            throw new ImpossibleMoveException("Конь так не ходит!");
+        }
         return new Cell[] {dest};
     }
 
