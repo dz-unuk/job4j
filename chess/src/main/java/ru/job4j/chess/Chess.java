e55d15e71b929b71405e131a27b23b00bfa09311
dz-unuk <d.unuchak@gmail.com>
2019-01-25 13:10:56 +0300

    Revert "Chess. Исправил ошибку - слон ходит нормально." Добавил исключение: FigureNotFoundException. Не понятно, как его можно вызвать. Все работает, ничего не поменялось.

--- chess/src/main/java/ru/job4j/chess/Chess.java
+++ chess/src/main/java/ru/job4j/chess/Chess.java
@@ -60,13 +60,20 @@ public class Chess extends Application {
         );
         rect.setOnMouseReleased(
                 event -> {
-                    if (logic.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()))) {
-                        rect.setX(((int) event.getX() / 40) * 40 + 5);
-                        rect.setY(((int) event.getY() / 40) * 40 + 5);
-                    } else {
+                    try {
+                        if (logic.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()))) {
+                            rect.setX(((int) event.getX() / 40) * 40 + 5);
+                            rect.setY(((int) event.getY() / 40) * 40 + 5);
+                        } else {
+                            rect.setX(((int) momento.getX() / 40) * 40 + 5);
+                            rect.setY(((int) momento.getY() / 40) * 40 + 5);
+                        }
+                    } catch (FigureNotFoundException e) {
                         rect.setX(((int) momento.getX() / 40) * 40 + 5);
                         rect.setY(((int) momento.getY() / 40) * 40 + 5);
+                        System.out.println(e);
                     }
+
                 }
         );
         return rect;
--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -19,19 +19,20 @@ public class Logic {
         this.figures[this.index++] = figure;
     }
 
-    public boolean move(Cell source, Cell dest) {
+    public boolean move(Cell source, Cell dest) throws FigureNotFoundException {
         boolean rst = false;
         int index = this.findBy(source);
-        if (index != -1) {
-            try {
-                Cell[] steps = this.figures[index].way(source, dest);
-                if (steps.length > 0 && steps[0].equals(dest)) {
-                    rst = true;
-                    this.figures[index] = this.figures[index].copy(dest);
-                }
-            } catch (ImpossibleMoveException ime) {
-                System.out.println(ime);
+        if (index == -1) {
+            throw new FigureNotFoundException("здесь нет фигуры");
+        }
+        try {
+            Cell[] steps = this.figures[index].way(source, dest);
+            if (steps.length > 0 && steps[0].equals(dest)) {
+                rst = true;
+                this.figures[index] = this.figures[index].copy(dest);
             }
+        } catch (ImpossibleMoveException ime) {
+            System.out.println(ime);
         }
         return rst;
     }
