66b45ba3bd92a254058d78c4c47c270f82c7a97f
dz-unuk <d.unuchak@gmail.com>
2019-04-25 12:04:37 +0300

    Priority - добавил

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -12,28 +12,13 @@ public class PriorityQueue {
      * @param task задача
      */
     public void put(Task task) {
-        int size = this.tasks.size();
 
-        int index = 0;
-        while (index < size
-                && this.tasks.get(index).getPriority() < task.getPriority()) {
-            index++;
-        }
+        int index = (int) this.tasks.stream()
+                .filter(task1 -> task1.getPriority() < task.getPriority())
+                .count();
+
         this.tasks.add(index, task);
-/*        boolean done = false;
-        if (size != 0) {
-            for (int i = 0; i < size; i++) {
-                if (this.tasks.get(i).getPriority() > task.getPriority()) {
-                    this.tasks.add(i, task);
-                    done = true;
-                }
-            }
-            if (!done) {
-                this.tasks.add(task);
-            }
-        } else {
-            this.tasks.add(task);
-        }*/
+
     }
 
     public Task take() {
